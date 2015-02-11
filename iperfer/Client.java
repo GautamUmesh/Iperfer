a880dffc8d2fb06e7ebb2cf85618c7f812c5cfb0
Gautam S Umesh <gautham.umesh@gmail.com>
2015-02-10 19:29:33 -0600

    Made minor changes to client

--- iperfer/Client.java
+++ iperfer/Client.java
@@ -18,15 +18,17 @@ public class Client {
 			socket = new Socket(host, port);
 			byte data[] = new byte[1000];
 			OutputStream outstream = socket.getOutputStream();
-			long endTime = System.currentTimeMillis() + time * 1000;
 			long numKbSent = 0;
-			while (System.currentTimeMillis() < endTime) {
+			long currentTime = 0;
+			long startTime = System.currentTimeMillis();
+			while (currentTime - startTime < (time * 1000)) {
 				outstream.write(data);
 				outstream.flush();
 				numKbSent += 1;
+			    currentTime = System.currentTimeMillis();
 			}
 			socket.close();
-			double rate = (numKbSent * 8.0) / (1000 * time);
+			double rate = (numKbSent * 8.0) / (currentTime - startTime);
 			System.out.println("sent=" + numKbSent + " KB rate="
 					+ String.format("%.3f", rate) + " Mbps");
 		} catch (Exception e) {
